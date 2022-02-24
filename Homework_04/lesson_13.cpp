459dd08066860c9e8dd9c85534443de0570ff5bb
Artur Smirnov <typeks@gmail.com>
2022-02-24 15:49:32 +0300

    Добавлена вторая часть 13го урока.

--- Homework_04/lesson_13.cpp
+++ Homework_04/lesson_13.cpp
@@ -35,45 +35,17 @@ int matrix[n][n] = {
 };
 int visited[n];
 
-void depthTravers(int st)
-{
-    Stack* stack = new Stack;
-    initStack(stack);
-    int r;
-    for (int k = 0; k < n; k++)
-    {
-        for (r = n - 1; r >= 0; r--)
-        {
-            if (matrix[st][r] == 1 && !visited[r])
-            {
-                push(stack, r);
-            }
-        }
-        std::cout << st;
-        visited[st] = 1;
-        st = pop(stack);
-        if (st == -1)
-        {
-            break;
-        }
-    }
-    while (pop(stack) >= 0)
-    {
-        pop(stack);
-    }
-    delete stack;
-}
+void depthTravers(int st);
 
-void resetArr()
-{
-    for (int k = 0; k < n; k++)
-    {
-        visited[k] = 0;
-    }
-}
+void resetArr();
 
-//// 01 //////////////////////////////
+//// 02 //////////////////////////////
 
+int countingConnections (int len, int vertex);
+
+void countAllVertices();
+
+void countTraversVertices(int st);
 
 int main()
 {
@@ -88,13 +60,24 @@ int main()
     std::cout << std::endl;
     depthTravers(1);
     resetArr();
+
     std::cout << std::endl << std::endl;
 
     //// 02  /////////////////////////////
 
-    std::cout << "######\n# 02 #\n######\n\nThe task is not done:\n\n";
+    std::cout << "######\n# 02 #\n######\n\nTravers:\n\n";
 
+    std::cout << "All vertices:\n\n";
+    countAllVertices();
+    std::cout << std::endl;
 
+    std::cout << "\nTravers end count vertices:\n\n";
+    countTraversVertices(0);
+    resetArr();
+    countTraversVertices(2);
+    resetArr();
+    countTraversVertices(1);
+    resetArr();
 
     std::cout << std::endl;
 
@@ -134,5 +117,100 @@ int pop(Stack* stack)
     return data;
 }
 
-// 02 ////////////////////////////////////
+void depthTravers(int st)
+{
+    Stack* stack = new Stack;
+    initStack(stack);
+    int r;
+    for (int k = 0; k < n; k++)
+    {
+        for (r = n - 1; r >= 0; r--)
+        {
+            if (matrix[st][r] == 1 && !visited[r])
+            {
+                push(stack, r);
+            }
+        }
+        std::cout << st;
+        visited[st] = 1;
+        st = pop(stack);
+        if (st == -1)
+        {
+            break;
+        }
+    }
+    while (pop(stack) >= 0)
+    {
+        pop(stack);
+    }
+    delete stack;
+}
 
+void resetArr()
+{
+    for (int k = 0; k < n; k++)
+    {
+        visited[k] = 0;
+    }
+}
+
+//// 02 //////////////////////////////
+
+int countingConnections (int len, int vertex)
+{
+    if (len == vertex)
+    {
+        return 0 + countingConnections(len - 1, vertex);
+    }
+    else if (matrix[len][vertex] == 1 && len >= 0)
+    {
+        return 1 + countingConnections(len - 1, vertex);
+    }
+    else if (len < 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 0 +countingConnections(len - 1, vertex);
+    }
+}
+
+void countAllVertices()
+{
+    for (int k = 0; k < n; k++)
+    {
+        std::cout << countingConnections(n,k);
+    }
+}
+
+void countTraversVertices(int st)
+{
+
+    Stack* stack = new Stack;
+    initStack(stack);
+    int r;
+    for (int k = 0; k < n; k++)
+    {
+        for (r = n - 1; r >= 0; r--)
+        {
+            if (matrix[st][r] == 1 && !visited[r])
+            {
+                push(stack, r);
+            }
+        }
+        std::cout << countingConnections(n,st);
+        visited[st] = 1;
+        st = pop(stack);
+        if (st == -1)
+        {
+            break;
+        }
+    }
+    while (pop(stack) >= 0)
+    {
+        pop(stack);
+    }
+    delete stack;
+    std::cout << std::endl;
+}
